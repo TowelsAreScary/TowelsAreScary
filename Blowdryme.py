7809a38c97a0f69019ef28f42b583bd6f2529fce
TowelsAreScary <101600813+TowelsAreScary@users.noreply.github.com>
2022-03-28 13:25:30 -0700

    Update Blowdryme.py

--- Blowdryme.py
+++ Blowdryme.py
@@ -55,24 +55,3 @@ while True:
             pixels.fill(Rne)
             pixels.show()
 
-while True:
-    if touch.value:
-        now=now+1
-        if (now>1):
-            now=0
-        print(now)
-    if(now==0):
-        pixels.fill(OFF)
-        pixels.show()
-    while (now==1):
-        for color in range(0,len(myCOLORS),1):
-            for light in range(0,12,1):
-                pixels[light]=myCOLORS[color]
-                pixels.show()
-            if touch.value:
-                now=now+1
-                if (now>1):
-                    now=0
-                print(now)
-                    
-    time.sleep(0.2)
